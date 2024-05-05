29d3a08ea58d5c6b3ea4e57d0252d85a264935cd
Aaryan <aryan.smart832@gmail.com>
2024-05-05 11:59:16 +0530

    replace with hassle-free

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,7 +9,7 @@ function Hero() {
     <div className="flex xl:flex-row flex-col gap-5 relative z-0  max-w-[1440px] mx-auto  ">
       <div className="flex-1 pt-36 sm:px-16 px-6 ">
         <h1 className=" 2xl:text-[72px] sm:text-[64px] text-[50px] font-extrabold ">
-          Find, book, or rent a car -quickly and easily!
+          Find, book, or rent a car -Hassle-free!
         </h1>
         <p className="text-[25px] font-light">
           Streamline your car rental experience with our effortless booking
